84b7eae3f584dcb868ec820b07617fb766ed9239
iwiedenm <iwiedenm@lion.lmu.edu>
2017-03-27 14:55:10 -0700

    nav

--- pages/nav.js
+++ pages/nav.js
@@ -2,13 +2,19 @@ document.getElementById("navMenu").innerHTML =
 '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
     '<ul class="nav navbar-nav">'+
         '<li>'+
-            '<a href="About.html">About</a>'+
+            '<a href="About.html">About Us</a>'+
         '</li>'+
         '<li>'+
-            '<a href="#">Services</a>'+
+            '<a href="Membership.html">Membership</a>'+
         '</li>'+
         '<li>'+
-            '<a href="#">Contact</a>'+
+            '<a href="Events.html">Events</a>'+
+        '</li>'+
+        '<li>'+
+            '<a href="AIMSLab.html">AIMS Lab</a>'+
+        '</li>'+
+        '<li>'+
+            '<a href="Contact.html">Contact</a>'+
         '</li>'+
     '</ul>'+
 '</div>';
