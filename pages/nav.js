ffcb11abf68712f4e68bd74a3d08c2fdf63e4193
cdeleon1 <cdeleon4@lion.lmu.edu>
2017-03-27 15:16:06 -0700

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -2,11 +2,10 @@ document.getElementById("navMenu").innerHTML =
 '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
     '<ul class="nav navbar-nav">'+
         '<li class="dropdown">'+
-            '<a class="dropdown-toggle" data-toggle="dropdown" href="About.html">About Us' + '<span class="caret">'+ '</span>'+ '</a>'+
+            '<a class="dropdown-toggle" data-toggle="dropdown" href="About.html">About Us' + '<span class="caret">' + '</span>'+ '</a>' +
     '<ul class="dropdown-menu">' +
-    '<li>' + '<a href="E-Board.html"> Executive Board</a>'+ '</li>'+
-    '</ul>' + 
-        '</li>'+
+    '<li>' + '<a href="E-Board.html">Executive Board</a>'+ '</li>' +
+    '</ul>' + '</li>' +
         '<li>'+
             '<a href="Membership.html">Membership</a>'+
         '</li>'+
