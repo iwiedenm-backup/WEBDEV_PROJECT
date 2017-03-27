1673311ac0f9ffeaa7f0c0b20456d4e71b3005e7
iwiedenm <iwiedenm@lion.lmu.edu>
2017-03-27 14:49:26 -0700

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -2,7 +2,7 @@ document.getElementById("navMenu").innerHTML =
 '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
     '<ul class="nav navbar-nav">'+
         '<li>'+
-            '<a href="#">About</a>'+
+            '<a href="About.html">About</a>'+
         '</li>'+
         '<li>'+
             '<a href="#">Services</a>'+
