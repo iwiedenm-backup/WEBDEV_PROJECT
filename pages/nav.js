6c6c0198a77127a90bf479a602e00883e6216920
iwiedenm <iwiedenm@lion.lmu.edu>
2017-03-27 14:37:38 -0700

    navbar

---
+++ pages/nav.js
@@ -0,0 +1,14 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
+    '<ul class="nav navbar-nav">'+
+        '<li>'+
+            '<a href="#">About</a>'+
+        '</li>'+
+        '<li>'+
+            '<a href="#">Services</a>'+
+        '</li>'+
+        '<li>'+
+            '<a href="#">Contact</a>'+
+        '</li>'+
+    '</ul>'+
+'</div>';
