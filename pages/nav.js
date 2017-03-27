604d1d71fff166f61e51033926322c744ad7a42a
cdeleon1 <cdeleon4@lion.lmu.edu>
2017-03-27 14:42:10 -0700

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -1,14 +1,20 @@
 document.getElementById("navMenu").innerHTML =
-'<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
-    '<ul class="nav navbar-nav">'+
-        '<li>'+
-            '<a href="#">About</a>'+
-        '</li>'+
-        '<li>'+
-            '<a href="#">Services</a>'+
-        '</li>'+
-        '<li>'+
-            '<a href="#">Contact</a>'+
-        '</li>'+
-    '</ul>'+
-'</div>';
+ '< div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
+                '<ul class="nav navbar-nav">'+
+                    ' <li> '+
+                        ' <a href="#">About Us</a> '+
+                    ' </li> '+
+                    ' <li> '+
+                     '    <a href="#">Membership</a> '+
+                    ' </li> '+
+  		      '<li> '+
+                        ' <a href="#">Events</a> '+
+                    ' </li> '+
+		   ' <li> '+
+                         '<a href="#">AIMS LAB</a> '+
+                     '</li> '+
+                     '<li> '+
+                       '  <a href="#">Contact</a> '+
+                    ' </li> '+
+                ' </ul> '+
+            '</div>';
