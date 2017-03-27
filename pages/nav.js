e843862b2279f2b1600fc0ad471681af73af685f
iwiedenm <iwiedenm@lion.lmu.edu>
2017-03-27 15:39:11 -0700

    updated home and nav

--- pages/nav.js
+++ pages/nav.js
@@ -2,11 +2,24 @@ document.getElementById("navMenu").innerHTML =
 '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">'+
     '<ul class="nav navbar-nav">'+
         '<li>'+
+            '<a href="index.html">Home</a>'+
+        '</li>'+
+        '<li>'+
             '<a href="About.html">About Us</a>'+
         '</li>'+
+
         '<li>'+
             '<a href="Membership.html">Membership</a>'+
         '</li>'+
+          '<li class="dropdown">'+
+            '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1'+
+            '<span class="caret"></span></a>'+
+            '<ul class="dropdown-menu">'+
+              '<li><a href="#">Page 1-1</a></li>'+
+              '<li><a href="#">Page 1-2</a></li>'+
+              '<li><a href="#">Page 1-3</a></li>'+
+            '</ul>'+
+          '</li>'+
         '<li>'+
             '<a href="Events.html">Events</a>'+
         '</li>'+
@@ -17,4 +30,63 @@ document.getElementById("navMenu").innerHTML =
             '<a href="Contact.html">Contact</a>'+
         '</li>'+
     '</ul>'+
+
 '</div>';
+
+
+// '<nav class="navbar navbar-toggleable-md navbar-light bg-faded">'+
+//   '<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">'+
+//     '<span class="navbar-toggler-icon"></span>'+
+//   '</button>'+
+//   '<a class="navbar-brand" href="#">Navbar</a>'+
+//   '<div class="collapse navbar-collapse" id="navbarNavDropdown">'+
+//     '<ul class="navbar-nav">'+
+//       '<li class="nav-item active">'+
+//         '<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>'+
+//       '</li>'+
+//       '<li class="nav-item">'+
+//         '<a class="nav-link" href="#">Features</a>'+
+//       '</li>'+
+//       '<li class="nav-item">'+
+//         '<a class="nav-link" href="#">Pricing</a>'+
+//       '</li>'+
+//       '<li class="nav-item dropdown">'+
+//         '<a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
+//           Dropdown link
+//         '</a>'+
+//         '<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">'+
+//           '<a class="dropdown-item" href="#">Action</a>'+
+//           '<a class="dropdown-item" href="#">Another action</a>'+
+//           '<a class="dropdown-item" href="#">Something else here</a>'+
+//         '</div>'+
+//       '</li>'+
+//     '</ul>'+
+//   '</div>'+
+// '</nav>';
+
+
+
+
+
+
+// '<nav class="navbar navbar-inverse">'+
+//   '<div class="container-fluid">'+
+//     '<div class="navbar-header">'+
+//       '<a class="navbar-brand" href="#">WebSiteName</a>'+
+//     '</div>'+
+//     '<ul class="nav navbar-nav">'+
+//       '<li class="active"><a href="#">Home</a></li>'+
+//       '<li class="dropdown">'+
+//         '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1'+
+//         '<span class="caret"></span></a>'+
+//         '<ul class="dropdown-menu">'+
+//           '<li><a href="#">Page 1-1</a></li>'+
+//           '<li><a href="#">Page 1-2</a></li>'+
+//           '<li><a href="#">Page 1-3</a></li>'+
+//         '</ul>'+
+//       '</li>'+
+//       '<li><a href="#">Page 2</a></li>'+
+//       '<li><a href="#">Page 3</a></li>'+
+//     '</ul>'+
+//   '</div>'+
+// '</nav>';
