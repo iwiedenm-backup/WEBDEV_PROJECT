4f3e12e8a68d24809f93168ada514abbba38f9de
cdeleon1 <cdeleon4@lion.lmu.edu>
2017-04-03 15:13:14 -0700

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -14,8 +14,8 @@ document.getElementById("navMenu").innerHTML =
             '<a href="https://docs.google.com/forms/d/1pFrWwW4VCTxlGCZnyqK1x7Yw4IwztFe_K3NfEIeDR0A/edit">Membership</a>'+
         '</li>'+
           '<li class="dropdown">'+
-            '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 '+
-            '<span class="caret"></span></a>'+
+            '<li class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 '+
+            '<span class="caret"></span></li>'+
             '<ul class="dropdown-menu">'+
               '<li><a href="http://www.google.com">Page 1-1</a></li>'+
               '<li><a href="#">Page 1-2</a></li>'+
