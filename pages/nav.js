11fc22a2155332b64e584c4d92e34b19e4ebbd86
cdeleon1 <cdeleon4@lion.lmu.edu>
2017-04-03 15:07:46 -0700

    Update nav.js

--- pages/nav.js
+++ pages/nav.js
@@ -13,7 +13,7 @@ document.getElementById("navMenu").innerHTML =
         '</li>'+
           '<li class="dropdown">'+
             '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 '+
-            '<span class="glyphicon glyphicon-search"></span></a>'+
+            '<span class="caret"></span></a>'+
             '<ul class="dropdown-menu">'+
               '<li><a href="http://www.google.com">Page 1-1</a></li>'+
               '<li><a href="#">Page 1-2</a></li>'+
